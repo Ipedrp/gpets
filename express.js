2d063f247ab449058fee4b04f06f4ad865205c6f
olvvikitor <victoroliveira.3002@gmail.com>
2024-05-24 00:39:16 -0300

    rotas criadas rotas para localização em tempo real

--- express.js
+++ express.js
@@ -1,19 +1,18 @@
 const express = require('express')
 
-// const moment = require('moment-timezone');
 
 const routerUsuario = require('./routes/usuario_router');
 const routerEndereco = require('./routes/endereco_router');
 const routerContatoUsuario = require('./routes/contato_usuario_router');
 const routerPets = require('./routes/pets_router');
 const routerAreaSegura = require('./routes/area_segura_router');
-const routerContatoPessoaEmergencia = require('./routes/contatos_pessoas_emergencia')
-const routerPessoaEmergencia = require('./routes/contatos_pessoas_emergencia')
+const routerContatoPessoaEmergencia = require('./routes/contatos_pessoas_emergencia');
+const routerPessoaEmergencia = require('./routes/contatos_pessoas_emergencia');
+
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-
 
 app.use('/api/usuario', routerUsuario);
 
@@ -31,38 +30,4 @@ app.use('/api/pessoaEmergencia', routerPessoaEmergencia);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-})
-
-
-const express = require('express')
-const routerUsuario = require('./routes/usuario_router');
-const routerEndereco = require('./routes/endereco_router');
-const routerContatoUsuario = require('./routes/contato_usuario_router');
-const routerPets = require('./routes/pets_router');
-const routerAreaSegura = require('./routes/area_segura_router');
-const routerContatoPessoaEmergencia = require('./routes/contatos_pessoas_emergencia')
-const routerLocalizacaoTempoReal = require('./routes/localizacao_tempo_real')
-
-const app = express();
-const port = 3000;
-
-app.use(express.json());
-
-
-app.use('/api/usuario', routerUsuario);
-
-app.use('/api/endereco', routerEndereco);
-
-app.use('/api/contatoUsuario', routerContatoUsuario);
-
-app.use('/api/pets', routerPets);
-
-app.use('/api/areaSegura', routerAreaSegura);
-
-app.use('/api/contatoPessoasEmergencia', routerContatoPessoaEmergencia);
-
-app.use('api/gps', routerLocalizacaoTempoReal);
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+});
